5ba3ab023303037986f70faad5a4bc5baa3d26cf
Ashish Singh <ashish1qwerty1@gmail.com>
2023-06-12 00:57:08 +0530

    Add side bar for mobile browser

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,12 +9,41 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="nav-bar">
+
+      <button className="menu-button" onClick={toggleMenu}>
+        <i className="fas fa-bars"></i>
+      </button>
+
       <a href="#home">Home</a>
       <a href="#about">About</a>
       <a href="#portfolio">Portfolio</a>
       <a href="#get-in-touch">Contact</a>
+
+      {showMenu && (
+        <div className="side-menu">
+          <a href="#home" onClick={toggleMenu}>
+            Home
+          </a>
+          <a href="#about" onClick={toggleMenu}>
+            About
+          </a>
+          <a href="#portfolio" onClick={toggleMenu}>
+            Portfolio
+          </a>
+          <a href="#get-in-touch" onClick={toggleMenu}>
+            Contact
+          </a>
+        </div>
+      )}
+
     </div>
   );
 };
