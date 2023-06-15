638abe32e65ee91c2707e8b0edcbfaef93c13b35
Ashish Singh <ashish1qwerty1@gmail.com>
2023-06-15 10:57:45 +0530

    Change home look

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,8 @@ import "./Home.css";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import landscapeImage from "../images/woman-with-tablet.jpg";
+// import landscapeImage from "../images/woman-with-tablet.jpg";
+import landscapeImage from "../images/blood-ffffff-bg.webp";
 // import portraitImage from "../images/portrait-girl-with-laptop.jpg";
 
 const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
