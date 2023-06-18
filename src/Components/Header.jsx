6fd72090fe55d2e1e0e48fd0407242b33959c4de
Ashish Singh <ashish1qwerty1@gmail.com>
2023-06-18 22:46:31 +0530

    Add ham menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,16 +5,44 @@
  * user scrolls so that they can constantly reach any part of your page.
  */
 import React from "react";
+import { useState } from "react";
 
 import "./Header.css";
 
+import "animate.css";
+
 const Header = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <div className="nav-bar">
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#portfolio">Portfolio</a>
-      <a href="#get-in-touch">Contact</a>
+    <div>
+      <div className="nav-bar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#get-in-touch">Contact</a>
+      </div>
+      
+      <div className="burger">
+        <button className="burger-btn" onClick={toggleMenu}>
+          <div className={`burger-bar ${showMenu ? 'open' : ''}`}></div>
+          <div className={`burger-bar ${showMenu ? 'open' : ''}`}></div>
+          <div className={`burger-bar ${showMenu ? 'open' : ''}`}></div>
+        </button>
+      </div>
+    
+
+      <div className={`nav-column ${showMenu ? 'open' : ''}`}>   
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#get-in-touch">Contact</a>
+      </div>
     </div>
   );
 };
